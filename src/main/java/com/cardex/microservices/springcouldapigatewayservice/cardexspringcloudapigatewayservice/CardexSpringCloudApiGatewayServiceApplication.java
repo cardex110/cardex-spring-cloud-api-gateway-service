ea49662db132e8de04765b946c976c937554c07e
Amine Baba <ababa@rbbn.com>
2021-12-05 11:22:38 -0500

    initial commit

---
+++ src/main/java/com/cardex/microservices/springcouldapigatewayservice/cardexspringcloudapigatewayservice/CardexSpringCloudApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cardex.microservices.springcouldapigatewayservice.cardexspringcloudapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardexSpringCloudApiGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardexSpringCloudApiGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cardex/microservices/springcouldapigatewayservice/cardexspringcloudapigatewayservice/CardexSpringCloudApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardex.microservices.springcouldapigatewayservice.cardexspringcloudapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardexSpringCloudApiGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
